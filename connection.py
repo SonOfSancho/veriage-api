1befe571286d7378bee35042bf3080b7315d89fd
EloiP <eloieloi001@gmail.com>
2026-03-02 15:32:58 +0100

    Primera version de uso de API.

---
+++ connection.py
@@ -0,0 +1,78 @@
+import requests
+import network_as_code as nac
+
+# Print full response
+
+key = "57bf2b3f70msh9bf3faea7510b98p175ea9jsn9a03050ce83f"
+prueba = "+99999991000"
+simprueba = "+34640197653"
+simdata = {
+"idDocument": "OJAZ00936",
+"name": "JOHN OPENTEST",
+"givenName": "JOHN", 
+"familyName": "OPENTEST",
+"familyNameAtBirth": "",
+"address": "DEL CLUB DEPORTIVO 1 28223",
+"streetName": "DEL CLUB DEPORTIVO",
+"streetNumber": "1",
+"postalCode": "28223",
+"region": "MADRID",
+"locality": "POZUELO DE ALARCON",
+"country": "ES",
+"birthdate":"1976-04-16",
+"email": "roberto.garcia@masorange.es",
+"gender": "MALE"}
+num = 240
+
+client = nac.NetworkAsCodeClient(token=key)
+
+def testcorrect(ans, res):
+    print("Correct")
+    print("Status Code: ", ans.status_code)
+    print("Raw Response: ", ans.text)
+    print("Resultado: ", res)
+
+def testincorrect(ans):
+    print("Incorrect")
+    print("Status Code: ", ans.status_code)
+
+def phoneverif(phone):
+    url = "https://network-as-code.p-eu.rapidapi.com/passthrough/camara/v1/number-verification/number-verification/v0/verify"
+    payload = {"phoneNumber": phone}
+    headers = {"Content-Type": "application/json", "x-rapidapi-host": "network-as-code.nokia.rapidapi.com", "x-rapidapi-key": key}
+    ans = requests.post(url, json=payload, headers=headers)
+    if (ans.status_code == 200):
+        data = ans.json()
+        res = data.get("verificationResult")
+        testcorrect(ans, res)#eliminar al terminar
+        return(res)
+    else:
+        testincorrect(ans)#eliminar despues
+        return (-1)
+
+
+def swapverif(phone):
+    url = "https://network-as-code.p-eu.rapidapi.com/passthrough/camara/v1/sim-swap/sim-swap/v0/check"
+    payload = {"phoneNumber": phone, "maxAge": num}
+    headers = {"Content-Type": "application/json", "x-rapidapi-host": "network-as-code.nokia.rapidapi.com", "x-rapidapi-key": key}
+    ans = requests.post(url, json=payload, headers=headers)
+    if (ans.status_code == 200):
+        data = ans.json()
+        res = data.get("swapped")
+        testcorrect(ans, res)#eliminar al final
+        return(res)
+    else:
+        testincorrect(ans)#eliminar al final
+        return(-1)
+
+def ageverif(phone, sim):
+    match = client.kyc.match_customer(phone_number=phone)
+    res = client.kyc.verify_age(age_threshhold=18)
+    print(res)
+    return(res)
+
+phoneverif(simprueba)
+
+swapverif(prueba)
+
+ageverif(prueba, simdata)
